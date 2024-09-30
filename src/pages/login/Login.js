eea74f230a3c8fa0121afe0776adf8180381851a
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-30 18:59:58 -0400

    Created Login Form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,41 @@
+import { useState } from 'react'
+
+// styles
 import styles from './Login.module.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: implement login logic using Firebase Auth
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
   )
 }
