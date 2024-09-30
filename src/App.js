51cb8c48f0fe948cd5fdd747027ba08bd22b3ee3
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-30 18:41:42 -0400

    Created base pages and setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+// pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={null}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+    )
+  );
+
   return (
     <div className="App">
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
