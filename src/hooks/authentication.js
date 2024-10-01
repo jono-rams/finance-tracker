8bac921f1951c7220a378e38b6cf9c54198386ce
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-30 20:37:36 -0400

    use Login action on Signup

--- src/hooks/authentication.js
+++ src/hooks/authentication.js
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { firebaseAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(null);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
     setError(null);
@@ -13,7 +15,6 @@ export const useSignup = () => {
     try {
       // signup user
       const res = await createUserWithEmailAndPassword(firebaseAuth, email, password);
-      console.log(res.user);
       
       if (!res) {
         throw new Error('Could not complete signup');
@@ -21,6 +22,9 @@ export const useSignup = () => {
 
       // update user profile
       await updateProfile(res.user, { displayName });
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
 
       setIsPending(false);
       setError(null);
