f0db3d47cb6c45e2beec803794afa35be67ea300
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-01 17:42:19 -0400

    Conditional Content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,11 @@ import { useLogout } from '../hooks/authentication'
 
 // styles
 import styles from './Navbar.module.css'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div>
@@ -13,11 +15,20 @@ export default function Navbar() {
         <nav className={styles.navbar}>
           <ul>
             <li className={styles.title}>myMoneyTracker</li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/signup'>Signup</Link></li>
-            <li>
-              <button className='btn' onClick={logout}>Logout</button>
-            </li>
+            {!user && (
+              <>
+                <li><Link to='/login'>Login</Link></li> 
+                <li><Link to='/signup'>Signup</Link></li>
+              </>  
+            )}                                  
+            {user && (
+              <>
+                <li>hello, {user.displayName}</li>
+                <li >
+                  <button className='btn' onClick={logout}>Logout</button>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
       </header>
